c329133fe3c131bd952e8d4239e6a14a66665d7a
Arnob Chakma <arnobchakma05@gmail.com>
2021-04-06 06:58:51 -0600

    server added

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u4kcg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njuwm.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
 
 const client = new MongoClient(uri, {
    useNewUrlParser: true,
